774507e4e54c91b4c9ae6f3018dd9374f498f4b1
Ashwani Senapati <senapatiashwani47@gmail.com>
2025-07-13 02:03:16 +0530

    fix:deploy

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ['lh3.googleusercontent.com','https://i5.walmartimages.com'],
+  },
 };
 
 export default nextConfig;
